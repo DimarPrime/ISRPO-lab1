586712cb7b4ad9c1795e978cece331893132c265
Dmitriy Medvedev <dimarprime@gmail.com>
2022-12-19 21:15:16 +0300

    Lab2 ISRPO Medvedev D.S.

---
+++ Wpflab2/Wpflab2/PeopleContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpflab2
+{
+    class PeopleContext : DbContext
+    {
+        public PeopleContext() : base("MyConnection")
+        { 
+
+        }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
