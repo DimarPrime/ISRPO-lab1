c6609797ae3f6d3609c2e8e555585dcd978d31a7
Dmitriy Medvedev <dimarprime@gmail.com>
2023-05-02 18:11:24 +0300

    lab4 edit final

---
+++ WpfLab4/Department.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace WpfLab4
+{
+    public class Department : INotifyPropertyChanged
+    {
+        string name;
+        ObservableCollection<Employee> employees;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                OnPropertyChanged("Name");
+                name = value;
+            }
+        }
+        public ObservableCollection<Employee> Employees
+        {
+            get { return employees; }
+            set
+            {
+                employees = value;
+                OnPropertyChanged("Players");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
