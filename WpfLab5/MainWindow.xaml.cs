7652020d18b1d376d88268b5743ff6147610fc15
Dmitriy Medvedev <dimarprime@gmail.com>
2023-05-02 21:00:03 +0300

    lab5 edit

---
+++ WpfLab5/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace WpfLab5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// коллекция для хранения данных из таблицы
+        /// </summary>
+        public ObservableCollection<Shop> Shop { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Shop = new Shop().Initialize();
+            // связываем коллекцию Shop и таблицу dgData
+            dgData.ItemsSource = Shop;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // создаем приложение ворд
+            Word.Application winword = new Word.Application();
+            //winword.Visible = true;
+
+            // добавляем документ
+            Word.Document document = winword.Documents.Add();
+
+            // добавляем параграф с номером накладной и выбранной датой
+            Word.Paragraph invoicePar = document.Content.Paragraphs.Add();
+            DateTime? selectDate = invoiceDate.SelectedDate;
+            if (selectDate != null)
+                invoicePar.Range.Text = string.Concat("Расходная накладная № ", invoiceNumber.Text, " от ", selectDate.Value.ToString("dd.MM.yyyy"));
+            else
+                invoicePar.Range.Text = string.Concat("Расходная накладная № ", invoiceNumber.Text);
+            invoicePar.Range.Font.Name = "Times new roman";
+            invoicePar.Range.Font.Size = 14;
+            invoicePar.Range.InsertParagraphAfter();
+
+            // добавляем параграф с поставщиком
+            Word.Paragraph providerPar = document.Content.Paragraphs.Add();
+            providerPar.Range.Text = string.Concat("Поставщик: ", PurchasertxtBox.Text);
+            providerPar.Range.Font.Name = "Times new roman";
+            providerPar.Range.Font.Size = 14;
+            providerPar.Range.InsertParagraphAfter();
+
+            // добавляем параграф с потребителем
+            Word.Paragraph customerPar = document.Content.Paragraphs.Add();
+            customerPar.Range.Text = "Покупатель: " + ProvidertxtBox.Text;
+            customerPar.Range.Font.Name = "Times new roman";
+            customerPar.Range.Font.Size = 14;
+            customerPar.Range.InsertParagraphAfter();
+
+            // формируем таблицу
+            // количество колонок - 5
+            // количество строк - nRows
+            int nRows = dgData.Items.Count;
+            Word.Table myTable = document.Tables.Add(customerPar.Range, nRows, 6);
+            myTable.Borders.Enable = 1;
+            // устанавливаем названия колонок
+            var headerRow = myTable.Rows[1].Cells;
+            headerRow[1].Range.Text = "№";
+            headerRow[2].Range.Text = "Товар";
+            headerRow[3].Range.Text = "Количество, в кг";
+            headerRow[4].Range.Text = "Цена, в руб.";
+            headerRow[5].Range.Text = "Сумма, в руб.";
+            headerRow[6].Range.Text = "Итого, в руб.";
+            // добавляем данные из таблицы в ворд
+            for (int i = 2; i < Shop.Count + 2; i++)
+            {
+                var dataRow = myTable.Rows[i].Cells;
+                dataRow[1].Range.Text = Shop[i - 2].Id.ToString();
+                dataRow[2].Range.Text = Shop[i - 2].Product;
+                dataRow[3].Range.Text = Shop[i - 2].Count.ToString();
+                dataRow[4].Range.Text = Shop[i - 2].Price.ToString();
+                dataRow[5].Range.Text = Shop[i - 2].Sum.ToString();
+                dataRow[6].Range.Text = Shop[i - 2].Total.ToString();
+            }
+            
+
+            // указываем в какой файл сохранить
+            // TODO - добавьте возможность выбора названия файла
+            // и места где его нужно сохранить
+            object filename = @"C:\Users\dimar\source\repos\word_z328_1.docx";
+            document.SaveAs(filename);
+            document.Close();
+            winword.Quit();
+        }
+    }
+
+}
---
+++ WpfLab5/Shop.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace WpfLab5
+{
+    public class Shop : INotifyPropertyChanged
+    {
+        int id;
+        string product;
+        int count;
+        int price;
+        int sum;
+        int total;
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string Product
+        {
+            get { return product; }
+            set
+            {
+                product = value;
+                OnPropertyChanged("Product");
+            }
+        }
+        public int Count
+        {
+            get { return count; }
+            set
+            {
+                count = value;
+                OnPropertyChanged("Count");
+            }
+        }
+        public int Price
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+        public int Sum
+        {
+            get { return sum; }
+            set
+            {
+                
+                sum = value;
+                                
+                OnPropertyChanged("Sum");
+            }
+        }
+         public int Total
+         {
+             get { return total; }
+             set
+             {
+
+                 total = value;
+
+                 OnPropertyChanged("Total");
+             }
+         }
+       
+     
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public ObservableCollection<Shop> Initialize()
+        {
+            return new ObservableCollection<Shop>()
+            {
+              new Shop
+              {
+                Id=1,
+                Product="Арбузы",
+                Count=3,
+                Price=16,
+                Sum=3*16,
+               
+                
+                
+                
+               // Total=3*16
+              },
+              new Shop
+              {
+                Id=2,
+                Product="Тыква",
+                Count=10,
+                Price=40,
+                Sum=10*40,
+                
+                
+               // Total=(3*16)+(10*40)
+              },
+              new Shop
+              {
+                Id=3,
+                Product="Яблоки",
+                Count=8,
+                Price=80,
+                Sum=8*80,
+                
+                
+                //Total=(3*16)+(10*40)+(8*80)
+
+              },
+              new Shop
+              {
+                Id=4,
+                Product="Лимоны",
+                Count=35,
+                Price=120,
+                Sum=35*120,
+                
+              },
+              new Shop
+              {
+                  Product="0",
+               Total=(3*16)+(10*40)+(8*80)+(35*120)
+              },
+
+
+           };
+
+        }
+    }
+
+}
