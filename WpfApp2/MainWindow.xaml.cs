f53075c173bc87c1906de55a0ecb58775d5ae452
Dmitriy Medvedev <dimarprime@gmail.com>
2023-05-08 19:21:58 +0300

    lab6final

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using WpfLibrary2;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Department> Departments { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Departments = new ObservableCollection<Department>()
+            {
+                new Department()
+                {
+                    Name = "Бухгалтерия",
+                    Employees = new ObservableCollection<Employee>()
+                    {
+                        new Employee()
+                        {
+                            Name = "Иван Иванович",
+                            Surname = "Иванов",
+                            Age = 31
+                        },
+                        new Employee()
+                        {
+                            Name = "Антон Антонович",
+                            Surname = "Антонов",
+                            Age = 32
+                        },
+                        new Employee()
+                        {
+                            Name = "Елизавета Сергеевна",
+                            Surname = "Филина",
+                            Age = 33
+                        }
+                    }
+                },
+                new Department()
+                {
+                    Name = "It-отдел",
+                    Employees = new ObservableCollection<Employee>()
+                    {
+                        new Employee()
+                        {
+                            Name = "Робин Анатольевич",
+                            Surname = "Вундервафлин",
+                            Age = 21
+                        },
+                        new Employee()
+                        {
+                            Name = "Максим Юрьевич",
+                            Surname = "Шлакоблокунь",
+                            Age = 22
+                        },
+                        new Employee()
+                        {
+                            Name = "Герман Петрович",
+                            Surname = "Винный",
+                            Age = 23
+                        }
+                    }
+                },
+                new Department()
+                {
+                    Name = "Отдел логистики",
+                    Employees = new ObservableCollection<Employee>()
+                    {
+                        new Employee()
+                        {
+                            Name = "Михаил Александрович",
+                            Surname = "Посылкин",
+                            Age = 41
+                        },
+                        new Employee()
+                        {
+                            Name = "Даниил Вольфович",
+                            Surname = "Лампочкин",
+                            Age = 42                        },
+                        new Employee()
+                        {
+                            Name = "Юрий Алексеевич",
+                            Surname = "Быстров",
+                            Age = 43
+                        }
+                    }
+                }
+            };
+            lbDepartments.ItemsSource = Departments;
+        }
+    }
+}
---
+++ WpfLibrary2/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace WpfLibrary2
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        string name;
+        string surname;
+        int age;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                surname = value;
+                OnPropertyChanged("Surname");
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
